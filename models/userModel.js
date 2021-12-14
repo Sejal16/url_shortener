daf7c9c0339c063e14c7a07d1e97344ac79590fb
sejal16 <sejal.augsut16@gmail.com>
2021-12-15 01:57:43 +0530

    connecting Db

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,14 @@
 const mongoose=require('mongoose');
 const emailValidator=require('email-validator');
+// <--------------Linking MongoDB to server---------->
+const db_link="mongodb+srv://admin:<ghJ8u9PPxH2K5s0a>@cluster0.jbvca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(db_link)
+.then(function(db){
+    console.log("Plan db connected");
+})
+.catch(function(err){
+    console.log(err);
+})
 
 
 // <-------------Schema Creation----------->
