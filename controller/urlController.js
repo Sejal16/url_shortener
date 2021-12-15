b20d21d9fbf57de8b6385ca8ab25ad0fdaa66c97
sejal16 <sejal.augsut16@gmail.com>
2021-12-15 22:26:09 +0530

    adding route for url shortening

---
+++ controller/urlController.js
@@ -0,0 +1,6 @@
+
+
+module.exports.shortUrl= function shortUrl(req,res)
+{
+    
+}
--- routers/urlRouter.js
+++ routers/urlRouter.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
 const urlRouter = express.Router();
+const{}=require("../controller/authController");
+const { shortUrl } = require('../controller/urlController');
+
+//url Shortening
+urlRouter.use(protectRoute)
+urlRouter
+    .route("/shortUrl")
+    .post(shortUrl)
+
 
 module.exports=urlRouter;
