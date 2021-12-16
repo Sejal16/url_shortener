609ff86ffe9a68cbe7bc496205ede01d4f1fa400
rahulgupta0902 <69122739+rahulgupta0902@users.noreply.github.com>
2021-12-16 12:04:15 +0530

    connected to index.ejs

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 app.use(express.json())
 app.listen(3000,()=>console.log("server satrted"))
 
@@ -9,3 +10,9 @@ const urlModel=require('./models/urlModel')
 const urlRouter=require('./routers/urlRouter')
 app.use("/",userRouter);
 app.use("/user",urlRouter);
+app.use(express.static(path.join(__dirname,'views')))
+app.use(express.urlencoded({extended :false}));
+app.set('view engine','ejs')
+app.get('/',async (req,res,next)=>{
+    res.render('index')
+})
